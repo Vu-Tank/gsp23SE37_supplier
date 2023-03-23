d8c80c405253312cac98060bb796af2c6579a2dd
NICE\huynh <vuha22.10.20@gmail.com>
2023-03-23 16:14:20 +0700

    pick file

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "assets/assets/logo/logo-color.svg": "eb74878552f19f4f5e2ed22f3c54b308",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/NOTICES": "ec610b1de37801267d0c80bdd261d16d",
+"assets/NOTICES": "7bc169a55a6336d71be562f91c98c589",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "b00363533ebe0bfdb95f3694d7647f6d",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "0a94bab8e306520dc6ae14c2573972ad",
@@ -33,9 +33,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2ff008adf0e6ee72d7b7243fe861f2be",
-"/": "2ff008adf0e6ee72d7b7243fe861f2be",
-"main.dart.js": "da0501c548bfbc7c9b859c049e1925b6",
+"index.html": "6cf05060535111f84db366508ddcf6a5",
+"/": "6cf05060535111f84db366508ddcf6a5",
+"main.dart.js": "074f7a306b6b0b4d4f2f2729e167fe02",
 "manifest.json": "bcc572d561e8ee6bf828d107ab9a4036",
 "version.json": "558f0fa2d82231d11315dd98af0f3cfe"
 };
--- lib/src/cubit/order_packing_video/order_packing_video_cubit.dart
+++ lib/src/cubit/order_packing_video/order_packing_video_cubit.dart
@@ -1,11 +1,8 @@
-import 'dart:typed_data';
-
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gsp23se37_supplier/src/model/api_response.dart';
 import 'package:gsp23se37_supplier/src/repositpries/order_repositories.dart';
-import 'package:gsp23se37_supplier/src/utils/utils.dart';
-import 'package:image_picker_web/image_picker_web.dart';
+import 'package:image_picker/image_picker.dart';
 // ignore: avoid_web_libraries_in_flutter
 import '../../repositpries/firebase_storage.dart';
 
@@ -19,36 +16,37 @@ class OrderPackingVideoCubit extends Cubit<OrderPackingVideoState> {
     try {
       // FilePickerResult? result =
       //     await FilePicker.platform.pickFiles(type: FileType.video);
-
-      // if (result != null) {
-      // PlatformFile file = result.files.first;
-      // var data = file.bytes;
-      Uint8List? videoData = await ImagePickerWeb.getVideoAsBytes();
-      if (videoData != null) {
-        String? url = await FirebaseStorageService()
-            .uploadFileVideo(videoData, Utils.createFile());
-        if (url != null) {
-          ApiResponse apiResponse = await OrderRepositories.updatePakingLink(
-              token: token, orderID: orderID, url: url);
-          if (apiResponse.isSuccess!) {
-            if (isClosed) return;
-            emit(OrderPackingVideoUpLoaded());
+      final ImagePicker picker = ImagePicker();
+      XFile? result = await picker.pickVideo(source: ImageSource.gallery);
+      if (result != null) {
+        // PlatformFile file = result.files.first;
+        // var data = file.bytes;
+        var data = await result.readAsBytes();
+        if (data != null) {
+          String? url =
+              await FirebaseStorageService().uploadFileVideo(data, result.name);
+          if (url != null) {
+            ApiResponse apiResponse = await OrderRepositories.updatePakingLink(
+                token: token, orderID: orderID, url: url);
+            if (apiResponse.isSuccess!) {
+              if (isClosed) return;
+              emit(OrderPackingVideoUpLoaded());
+            } else {
+              if (isClosed) return;
+              emit(OrderPackingVideoLoadFailed(apiResponse.msg!));
+            }
           } else {
             if (isClosed) return;
-            emit(OrderPackingVideoLoadFailed(apiResponse.msg!));
+            emit(const OrderPackingVideoLoadFailed('Lỗi chọn lại'));
           }
         } else {
           if (isClosed) return;
-          emit(const OrderPackingVideoLoadFailed('Lỗi chọn lại'));
+          emit(const OrderPackingVideoLoadFailed('Chọn Video'));
         }
       } else {
         if (isClosed) return;
         emit(const OrderPackingVideoLoadFailed('Chọn Video'));
       }
-      // } else {
-      //   if (isClosed) return;
-      //   emit(const OrderPackingVideoLoadFailed('Chọn Video'));
-      // }
     } catch (e) {
       if (isClosed) return;
       emit(OrderPackingVideoLoadFailed(e.toString()));
