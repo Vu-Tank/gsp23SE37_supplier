14dcdeb396e5c8573c6554637451663d6c169dcd
NICE\huynh <vuha22.10.20@gmail.com>
2023-03-26 10:09:25 +0700

    rebuild

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2a957044800aa69b771fd0b907acfeaf",
-"/": "2a957044800aa69b771fd0b907acfeaf",
-"main.dart.js": "8cad06d5a697022d005cd8191656f597",
+"index.html": "9f1fda820c8a29534d708069d938484f",
+"/": "9f1fda820c8a29534d708069d938484f",
+"main.dart.js": "b34557b2e9967621cb71e4862a0a31d8",
 "manifest.json": "bcc572d561e8ee6bf828d107ab9a4036",
 "version.json": "558f0fa2d82231d11315dd98af0f3cfe"
 };
