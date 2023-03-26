c3c96f275c93acc6381023c76c4afa2c52581cf4
NICE\huynh <vuha22.10.20@gmail.com>
2023-03-26 08:49:24 +0700

    remove top

--- lib/src/page/dashboard_page.dart
+++ lib/src/page/dashboard_page.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:gsp23se37_supplier/src/cubit/item_hot/item_hot_cubit.dart';
-import 'package:gsp23se37_supplier/src/model/item/item.dart';
-import 'package:gsp23se37_supplier/src/page/item/item_detail_widget.dart';
 import 'package:gsp23se37_supplier/src/page/store/cash_flow_dialog.dart';
 import 'package:gsp23se37_supplier/src/page/store/store_withdrawal_dialog.dart';
 import 'package:gsp23se37_supplier/src/utils/app_style.dart';
 import 'package:gsp23se37_supplier/src/utils/my_dialog.dart';
-import 'package:gsp23se37_supplier/src/widget/bloc_load_failed.dart';
 import 'package:intl/intl.dart';
 
 import '../bloc/auth/auth_bloc.dart';
@@ -145,94 +141,94 @@ class _DashboardPageState extends State<DashboardPage> {
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: BlocProvider(
-                        create: (context) => ItemHotCubit()
-                          ..loadHotItem(
-                              token: user.token, storeID: store.storeID),
-                        child: BlocBuilder<ItemHotCubit, ItemHotState>(
-                          builder: (context, state) {
-                            if (state is ItemHotLoaded) {
-                              if (state.list.isEmpty) {
-                                return Text(
-                                  'Không có dữ liệu',
-                                  style: AppStyle.h2,
-                                );
-                              } else {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ConstrainedBox(
-                                    constraints: const BoxConstraints(
-                                        minHeight: 200,
-                                        minWidth: 200,
-                                        // maxHeight: 300,
-                                        maxWidth: 300),
-                                    child: SingleChildScrollView(
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: List.generate(
-                                            state.list.length, (index) {
-                                          Item item = state.list[index];
-                                          return Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                vertical: 8.0),
-                                            child: InkWell(
-                                              onTap: () {
-                                                showDialog(
-                                                  context: context,
-                                                  builder: (context) =>
-                                                      ItemDetailWidget(
-                                                          itemId: item.itemID,
-                                                          token: user.token,
-                                                          edit: false),
-                                                );
-                                              },
-                                              child: Row(
-                                                children: [
-                                                  SizedBox(
-                                                    height: 50,
-                                                    width: 50,
-                                                    child: Image.network(
-                                                        item.item_Image),
-                                                  ),
-                                                  Expanded(
-                                                    child: Text(
-                                                      item.name,
-                                                      style: AppStyle.h2,
-                                                      maxLines: 1,
-                                                      overflow:
-                                                          TextOverflow.ellipsis,
-                                                    ),
-                                                  )
-                                                ],
-                                              ),
-                                            ),
-                                          );
-                                        }),
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              }
-                            } else if (state is ItemHotFailed) {
-                              return blocLoadFailed(
-                                msg: state.msg,
-                                reload: () {
-                                  context.read<ItemHotCubit>().loadHotItem(
-                                      token: user.token,
-                                      storeID: store.storeID);
-                                },
-                              );
-                            } else {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                    ),
+                    // Expanded(
+                    //   child: BlocProvider(
+                    //     create: (context) => ItemHotCubit()
+                    //       ..loadHotItem(
+                    //           token: user.token, storeID: store.storeID),
+                    //     child: BlocBuilder<ItemHotCubit, ItemHotState>(
+                    //       builder: (context, state) {
+                    //         if (state is ItemHotLoaded) {
+                    //           if (state.list.isEmpty) {
+                    //             return Text(
+                    //               'Không có dữ liệu',
+                    //               style: AppStyle.h2,
+                    //             );
+                    //           } else {
+                    //             return Padding(
+                    //               padding: const EdgeInsets.all(8.0),
+                    //               child: ConstrainedBox(
+                    //                 constraints: const BoxConstraints(
+                    //                     minHeight: 200,
+                    //                     minWidth: 200,
+                    //                     // maxHeight: 300,
+                    //                     maxWidth: 300),
+                    //                 child: SingleChildScrollView(
+                    //                   child: Column(
+                    //                     crossAxisAlignment:
+                    //                         CrossAxisAlignment.start,
+                    //                     children: List.generate(
+                    //                         state.list.length, (index) {
+                    //                       Item item = state.list[index];
+                    //                       return Padding(
+                    //                         padding: const EdgeInsets.symmetric(
+                    //                             vertical: 8.0),
+                    //                         child: InkWell(
+                    //                           onTap: () {
+                    //                             showDialog(
+                    //                               context: context,
+                    //                               builder: (context) =>
+                    //                                   ItemDetailWidget(
+                    //                                       itemId: item.itemID,
+                    //                                       token: user.token,
+                    //                                       edit: false),
+                    //                             );
+                    //                           },
+                    //                           child: Row(
+                    //                             children: [
+                    //                               SizedBox(
+                    //                                 height: 50,
+                    //                                 width: 50,
+                    //                                 child: Image.network(
+                    //                                     item.item_Image),
+                    //                               ),
+                    //                               Expanded(
+                    //                                 child: Text(
+                    //                                   item.name,
+                    //                                   style: AppStyle.h2,
+                    //                                   maxLines: 1,
+                    //                                   overflow:
+                    //                                       TextOverflow.ellipsis,
+                    //                                 ),
+                    //                               )
+                    //                             ],
+                    //                           ),
+                    //                         ),
+                    //                       );
+                    //                     }),
+                    //                   ),
+                    //                 ),
+                    //               ),
+                    //             );
+                    //           }
+                    //         } else if (state is ItemHotFailed) {
+                    //           return blocLoadFailed(
+                    //             msg: state.msg,
+                    //             reload: () {
+                    //               context.read<ItemHotCubit>().loadHotItem(
+                    //                   token: user.token,
+                    //                   storeID: store.storeID);
+                    //             },
+                    //           );
+                    //         } else {
+                    //           return const Center(
+                    //             child: CircularProgressIndicator(),
+                    //           );
+                    //         }
+                    //       },
+                    //     ),
+                    //   ),
+                    // ),
                   ]),
                 ),
               ))
